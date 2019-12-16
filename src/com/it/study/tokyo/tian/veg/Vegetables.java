ac4923c9a952b24ac7b7201594fea0a4d3a88d43
Tian, Wen <ts-wen.tian@rakuten.com>
2019-12-16 11:06:21 +0900

    add Vegetables

---
+++ src/com/it/study/tokyo/tian/veg/Vegetables.java
@@ -0,0 +1,23 @@
+package com.it.study.tokyo.tian.veg;
+
+/**
+ * @author ts-wen.tian
+ */
+public class Vegetables {
+    /**
+     * vegetable name
+     */
+    private String name;
+
+    public Vegetables(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
