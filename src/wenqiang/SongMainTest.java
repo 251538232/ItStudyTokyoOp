c6158a69249da62924d16e7b304ff334f9d3cbf9
songwenqiang369 <song.wenqiang0229@gmail.com>
2019-12-18 23:19:24 +0900

    add SongMainTest class

---
+++ src/wenqiang/SongMainTest.java
@@ -0,0 +1,78 @@
+package wenqiang;
+
+public class SongMainTest {
+    package com.skypan.xihongshichaojidan;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+    public class XiHongShiChaoJiDan {
+        String tomato;
+        String egg;
+        String oil;
+        String tiaoliao;
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("do you have tomato? please input Y/N");
+            String a = br;
+            if a = "Y";
+            public void wash ();
+            //清洗西红柿
+            public void kill ();
+            //切西红柿
+            public void pour ();
+            //倒入西红柿
+            if a = "N";
+            public void finish ();
+            //结束炒菜
+
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("do you have egg? please input Y/N");
+            String b = br;
+            if b = "Y";
+            public void peel ();
+            //给鸡蛋去皮
+            public void stir ();
+            //搅拌鸡蛋
+            public void pour ();
+            //倒入鸡蛋液
+            if b = "N";
+            public void finish ();
+            //结束炒菜
+
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("do you have oil? please input Y/N");
+            String c = br;
+            if c = "Y";
+            public void pour ();
+            //倒入油
+            public void heatup ();
+            //加热油
+            if b = "N";
+            public void finish ();
+            //结束炒菜
+
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("do you have tiaoweiliao? please input Y/N");
+            String d = br;
+            if d = "Y";
+            public void pour ();
+            //倒入调味料
+            if d = "N";
+            public void finish ();
+            //结束炒菜
+
+            public void fry ();
+            //开始炒菜
+
+            System.out.println("西红柿炒鸡蛋做好了");
+        }
+    }
+
+}
