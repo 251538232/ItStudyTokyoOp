5dce80d77e4218f5d84728b39ce79964cc0eabb4
Tian, Wen <ts-wen.tian@rakuten.com>
2019-12-19 09:27:43 +0900

    add review

--- src/wenqiang/SongMainTest.java
+++ src/wenqiang/SongMainTest.java
@@ -1,6 +1,14 @@
 package wenqiang;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SongMainTest {
+    /**
+     * 存在编译错误的文件需要把文件修改完成后再上传，保证远程仓库代码是没有编译错误的
+     * 整体看来确实问题较多
+     */
     package com.skypan.xihongshichaojidan;
 
         import java.io.BufferedReader;
@@ -18,7 +26,19 @@ public class SongMainTest {
                     new BufferedReader(new InputStreamReader(System.in));
             System.out.println("do you have tomato? please input Y/N");
             String a = br;
+            /**
+             * if语句错误
+             *
+             * if ( 条件式){
+             *     xxxxx
+             * }
+             */
             if a = "Y";
+
+            /**
+             * 类中的方法需要有实现 区别与interface中的方法，比如 wash finish等
+             * 另外看你方法的定义也写在main方法中，这是连编译都不会通过的
+             */
             public void wash ();
             //清洗西红柿
             public void kill ();
@@ -41,6 +61,9 @@ public class SongMainTest {
             public void pour ();
             //倒入鸡蛋液
             if b = "N";
+            /**
+             * 同一个类中除重载以外，不可存在两个同样的方法
+             */
             public void finish ();
             //结束炒菜
 
@@ -51,6 +74,9 @@ public class SongMainTest {
             if c = "Y";
             public void pour ();
             //倒入油
+            /**
+             * 知道写注释还是值得表扬的
+             */
             public void heatup ();
             //加热油
             if b = "N";
