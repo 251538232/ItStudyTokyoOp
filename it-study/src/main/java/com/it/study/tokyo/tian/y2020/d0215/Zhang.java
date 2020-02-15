5e4893de80df68bf40f01fb35f53dcff8b699ac5
lizaohuizi <lizaohuizi@gamil.com>
2020-02-15 14:41:11 +0900

    zhanghui

--- it-study/src/main/java/com/it/study/tokyo/tian/y2020/d0215/Zhang.java
+++ it-study/src/main/java/com/it/study/tokyo/tian/y2020/d0215/Zhang.java
@@ -1,11 +1,33 @@
 package com.it.study.tokyo.tian.y2020.d0215;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Zhang {
 
     public static void main(String[] args) {
         String text = "123,45,6,7";
-        String text1 = "123,45,6,7";
 
+
+                char[] chars = text.toCharArray();
+                List<Integer> ints = new ArrayList<>();
+
+                String tmp = "";
+                for (char c : chars) {
+                    if (c != ',') {
+                        tmp = tmp + c;
+                    }
+                    if (c == ',') {
+                        ints.add(Integer.parseInt(tmp));
+                        tmp = "";
+                    }
+                }
+                if (!"".equals(tmp)) {
+                    ints.add(Integer.parseInt(tmp));
+                }
+
+                System.out.println(ints.toString());
 
     }
 }
