0e463a071d31f8fe4ba6bbe053d0cdbc3e3dabd7
shaoxianxiang <276569692@qq.com>
2020-02-15 15:55:01 +0900

    paiza

---
+++ it-study/src/main/java/com/it/study/tokyo/tian/y2020/Main.java
@@ -0,0 +1,27 @@
+package com.it.study.tokyo.tian.y2020;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 標準入力から値を取得してinput_lineに入れる
+        String inputLine = sc.nextLine();
+        int di = Integer.parseInt(inputLine);
+        String[] heights = sc.nextLine().split(" ");
+        int smallHeight = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int i1 = Integer.parseInt(heights[i]);
+            if (smallHeight == 0) {
+                smallHeight = i1;
+                continue;
+            }
+            if (smallHeight > i1) {
+                smallHeight = i1;
+            }
+        }
+        // 取得した値を標準出力するために"XXXXXX"をどう書き換える？
+        System.out.println(smallHeight * di);
+    }
+}
--- it-study/src/main/java/com/it/study/tokyo/tian/y2020/d0215/Wang.java
+++ it-study/src/main/java/com/it/study/tokyo/tian/y2020/d0215/Wang.java
@@ -1,32 +1,39 @@
 package com.it.study.tokyo.tian.y2020.d0215;
 
-import org.apache.tomcat.util.http.fileupload.MultipartStream;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Wang {
     public static void main(String[] args) {
-
-        String text = "123,45,6,7";
-        char[] chars = text.toCharArray();
-        List<Integer> ints = new ArrayList<>();
-
-        String tmp = "";
-        for (char c : chars) {
-            if (c != ',') {
-                tmp = tmp + c;
-            }
-            if (c == ',') {
-                ints.add(Integer.parseInt(tmp));
-                tmp = "";
+        String text = "123,45.6,7,8,90.16,17,18,190.";
+        List<Integer> strings = new ArrayList<>();
+        String[] sp = text.split(",");
+        for (String s : sp) {
+            String[] split = s.split("\\.");
+            for (String s1 : split) {
+                strings.add(Integer.parseInt(s1));
             }
         }
-        if (!"".equals(tmp)) {
-            ints.add(Integer.parseInt(tmp));
-        }
-
-        System.out.println(ints.toString());
+        System.out.println(strings);
+        String.valueOf(true);
+//        char[] chars = text.toCharArray();
+//        List<Integer> ints = new ArrayList<>();
+//
+//        String tmp = "";
+//        for (char c : chars) {
+//            if (c != ',') {
+//                tmp = tmp + c;
+//            }
+//            if (c == ',') {
+//                ints.add(Integer.parseInt(tmp));
+//                tmp = "";
+//            }
+//        }
+//        if (!"".equals(tmp)) {
+//            ints.add(Integer.parseInt(tmp));
+//        }
+//
+//        System.out.println(ints.toString());
     }
 
 }
