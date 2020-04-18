1fcd696b79168221bd10daf5d4b532eb8eaa7512
shaoxianxiang <276569692@qq.com>
2020-04-18 21:48:34 +0900

    代码提交3

---
+++ it-study/src/main/java/com/it/study/tokyo/tian/y2020/d0418/TestStudy.java
@@ -0,0 +1,26 @@
+package com.it.study.tokyo.tian.y2020.d0418;
+
+/**
+ * 20200418 作业
+ */
+public class TestStudy {
+    private String value;
+
+    public String getStr(String str, int i) {
+        return str + i;
+    }
+
+    public float getFloat(float f, int i) {
+        return f * i;
+    }
+
+    public TestStudy getTestStudy(float f, int i) {
+        TestStudy testStudy = new TestStudy();
+        testStudy.value = String.valueOf(f * i);
+        return testStudy;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
