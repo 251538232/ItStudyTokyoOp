b50aabe447a0b73df46429216eb580a63e8a926b
shaoxianxiang <276569692@qq.com>
2020-04-18 20:41:30 +0900

    代码提交2

--- it-study/src/main/test/java/com/it/service/TestServiceTest.java
+++ it-study/src/main/test/java/com/it/service/TestServiceTest.java
@@ -1,21 +1,46 @@
 package com.it.service;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TestServiceTest {
 
+    TestService testService = new TestService();
 
+    @Before
+    public void setUp() throws Exception {
 
-    @Test
-    public void testSend() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
-    public void multi() {
-        TestService testService = new TestService();
-        String multi = testService.multi(100);
-        assertEquals("10000" ,multi);
+    public void sendTest() throws Exception {
+        // "aaa222"
+        String s = testService.send("aaa");
+        assertEquals("aaa222", s);
+    }
+
+    @Test
+    public void multiTest() {
+        String s = testService.multi(100);
+        assertEquals("10000", s);
+    }
+
+    @Test
+    public void multiTest2() {
+        String s = testService.multi(10);
+        assertEquals("123", s);
+    }
+
+    @Test
+    public void testSendTest() throws Exception {
+        String s = testService.testSend();
+        assertEquals("321", s);
     }
 }
