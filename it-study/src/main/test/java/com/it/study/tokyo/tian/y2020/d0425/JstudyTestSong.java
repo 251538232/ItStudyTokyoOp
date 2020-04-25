7c55ef63d5a1fea2bda5b10d2f8227d320c2ca05
shaoxianxiang <276569692@qq.com>
2020-04-25 21:38:57 +0900

    homework please study harder and harder

---
+++ it-study/src/main/test/java/com/it/study/tokyo/tian/y2020/d0425/JstudyTestSong.java
@@ -0,0 +1,18 @@
+package com.it.study.tokyo.tian.y2020.d0425;
+
+import org.junit.Test;
+
+public class JstudyTestSong {
+
+    @Test
+    public void getMulti() {
+    }
+
+    @Test
+    public void testGetMulti() {
+    }
+
+    @Test
+    public void setMyBean() {
+    }
+}
