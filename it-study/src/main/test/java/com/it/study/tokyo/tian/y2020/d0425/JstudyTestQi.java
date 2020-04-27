f52a0986bbbf0742d6ee8ba31355dd3bb478e16f
ts-wen.tian <ts-wen.tian@rakuten.com>
2020-04-27 10:08:48 +0900

    wsq

--- it-study/src/main/test/java/com/it/study/tokyo/tian/y2020/d0425/JstudyTestQi.java
+++ it-study/src/main/test/java/com/it/study/tokyo/tian/y2020/d0425/JstudyTestQi.java
@@ -2,17 +2,29 @@ package com.it.study.tokyo.tian.y2020.d0425;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class JstudyTestQi {
 
     @Test
     public void getMulti() {
+        Jstudy jstudy = new Jstudy();
+        int i = jstudy.getMulti(1, 1);
+        assertEquals(2, i);
     }
 
     @Test
     public void testGetMulti() {
+        Jstudy jstudy = new Jstudy();
+        float f = jstudy.getMulti(1.0f, 1.0f);
+        assertEquals(2, (int) f);
     }
 
     @Test
     public void setMyBean() {
+        Jstudy jstudy = new Jstudy();
+        jstudy.setMyBean();
+        assertEquals(0, jstudy.getMyBean().getLen());
+        assertEquals("test", jstudy.getMyBean().getText());
     }
 }
