a10e68f37ffd7c1b148c4d907d62fcf1633a671e
shaoxianxiang <276569692@qq.com>
2020-05-09 20:27:19 +0900

    mock-2.test

---
+++ it-study/src/main/test/java/com/it/study/tokyo/tian/y2020/d0509/MyStudyTest.java
@@ -0,0 +1,12 @@
+package com.it.study.tokyo.tian.y2020.d0509;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyStudyTest {
+
+    @Test
+    public void setMyName() {
+    }
+}
